d0d4dc7d36892f3a70d573894466ce801c5c91a4
Archer Hasbany <archerhasbany@gmail.com>
2024-03-25 16:27:59 -0500

    Created swing chart

--- Archer Golf/Views/SwingChart.swift
+++ Archer Golf/Views/SwingChart.swift
@@ -10,28 +10,46 @@ import Charts
 import SwiftData
 
 struct SwingChart: View {
-    var session: SwingSession
+    @Binding var swing: Swing
     
     var body: some View {
+        let accelXData = swing.accelX.enumerated().map { index, element in
+            SensorReading(index: index , reading: element)
+        }
+        let accelYData = swing.accelY.enumerated().map { index, element in
+            SensorReading(index: index , reading: element)
+        }
+        let accelZData = swing.accelZ.enumerated().map { index, element in
+            SensorReading(index: index , reading: element)
+        }
+        let chartData = [ (axis: "Accel X", data: accelXData),
+                      (axis: "Accel Y", data: accelYData),
+                      (axis: "Accel Z", data: accelZData)]
         
         Chart {
-            ForEach(Array(session.accelX.enumerated()), id: \.offset) { index, accelx in
-                LineMark(
-                    x: .value("time", index),
-                    y: .value("Deg/s", accelx)
-                )
+            ForEach(chartData, id: \.axis) { series in
+                ForEach(series.data) { item in
+                    LineMark(
+                        x: .value("x", item.index),
+                        y: .value("y", item.reading)
+                    )
+                }
+                .foregroundStyle(by: .value("Axis", series.axis))
             }
-            
-//            ForEach(Array(session.accelY.enumerated()), id: \.offset) { index, accelx in
-//                LineMark(
-//                    x: .value("time", index),
-//                    y: .value("Deg/s", accelx)
-//                )
-//            }
         }
-        .frame(height: 300)
         .padding()
+        .chartYAxis {
+            AxisMarks(position: .leading)
+        }
+        .chartXAxisLabel("ms")
+        .chartYAxisLabel("Gs")
     }
+}
+
+struct SensorReading: Identifiable {
+    var id = UUID()
+    var index: Int
+    var reading: Double
 }
 
 #Preview {
@@ -39,13 +57,22 @@ struct SwingChart: View {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Swing.self, SwingSession.self, configurations: config)
         
-        let exampleSwing = Swing(faceAngle: 0.0, swingSpeed: 1.2, swingPath: 2.2, backSwingTime: 1.1, downSwingTime: 0.7)
+        @State var exampleSwing: Swing = Swing(faceAngle: 0.0, swingSpeed: 1.2, swingPath: 2.2, backSwingTime: 1.1, downSwingTime: 0.7)
+        
+        exampleSwing.accelX = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        exampleSwing.accelY = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        exampleSwing.accelZ = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        
+        exampleSwing.gyroX = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        exampleSwing.gyroY = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        exampleSwing.gyroZ = (1...100).map { _ in Double.random(in: -6.0...6.0) }
+        
         var exampleSession = SwingSession()
         exampleSession.accelX = [0.2, 0.1, 0.3]
         
         exampleSession.swings.append(exampleSwing)
         
-        return SwingChart(session: exampleSession)
+        return SwingChart(swing: $exampleSwing)
             .modelContainer(container)
     } catch {
         fatalError("Failed to create model container.")
